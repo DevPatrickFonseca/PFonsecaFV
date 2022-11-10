8724dc727230b7992e2ab8c9ee21a17a915353e4
Patrick Fonseca <107743751+PFonsecaFV@users.noreply.github.com>
2022-11-09 23:01:32 -0300

    Organizando/Atualizando - Diretórios e arquivos

--- ani.js
+++ ani.js
@@ -1,3 +1,5 @@
 document.querySelector( "#retrobg-sun" ).onclick = () => {
   document.querySelector( "#retrobg" ).classList.toggle( "retrobg-shutdown" );
 };
+
+// Reference - About the author: Warren Davies is a front end developer based in the UK. You can find more from him at https://warrendavies.net
